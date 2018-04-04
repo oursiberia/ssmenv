28bad95b73c51f202cf5b172c837775414967d6a
Bryan Swift <bryan@siberia.io>
2018-04-04 19:02:43 -0400

    Add `KEY` and `VALUE` constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const ACCESS_KEY_ID = 'AWS_ACCESS_KEY';
 export const CONFIG_FILE = '.ssmenv';
+export const KEY = 'KEY';
 export const ROOT_PATH = 'ROOT_PATH';
 export const SECRET_KEY_ID = 'AWS_SECRET_KEY';
 export const STAGE = 'STAGE';
+export const VALUE = 'VALUE';
