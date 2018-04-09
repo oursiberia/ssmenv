1ceef35f0c60930e7d8ec09d1cfec449c916614d
Bryan Swift <bryan@siberia.io>
2018-04-09 15:58:13 -0400

    feat(env:list): Create `env:list` command
    The `env:list` command lists the full path to all know environments.
    
    Refs #8

---
+++ src/commands/env/list.ts
@@ -0,0 +1,49 @@
+import { Command } from '@oclif/command';
+import { args as Parser } from '@oclif/parser';
+
+import { AwsSsmProxy } from '../../environment/AwsSsmProxy';
+import { make as makeExample } from '../../example';
+import { getDirectEnvironment } from '../../projectConfig';
+
+export class EnvList extends Command {
+  static description = [
+    'List the known stages or environments, ignoring configured `rootPath`.',
+    'A stage or environment is considered any path with a direct child path holding a value.',
+  ].join('\n');
+
+  static examples = [
+    makeExample([
+      `# List out the full paths to all known stages.`,
+      `$ ssmenv env:list`,
+      `/client/project/dev`,
+      `/client/project/production`,
+      `/client/project/test`,
+      `/otherclient/test_project/production`,
+      `/otherproject/dev`,
+    ]),
+  ];
+
+  async run() {
+    const proxy = await getDirectEnvironment();
+    const request = {
+      Path: '/',
+      Recursive: true,
+    };
+    const results = await proxy.getParametersByPath(request);
+    const parameters = results.Parameters || [];
+    parameters
+      .map(param => {
+        const lastSlash = param.Name!.lastIndexOf('/');
+        return param.Name!.slice(0, lastSlash);
+      })
+      .reduce((paths: string[], next: string) => {
+        if (paths.includes(next)) {
+          return paths;
+        } else {
+          return [...paths, next];
+        }
+      }, [])
+      .sort()
+      .forEach(path => this.log(path));
+  }
+}
