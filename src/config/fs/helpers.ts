0bf6856b7a6a86cf892c3668e32dfea20da9b586
Bryan Swift <bryan@siberia.io>
2018-04-13 11:56:20 -0400

    Change `getAwsConfig` and `getProjectConfig` to take fileName params

--- src/config/fs/helpers.ts
+++ src/config/fs/helpers.ts
@@ -11,20 +11,28 @@ import { ConfigFile } from './ConfigFile';
 /**
  * Create a new `ConfigFile` for the given configuration path.
  * @param pathToConfig from which the aws config will be read.
+ * @param fileName of the config file within `pathToConfig`
  * @returns a handler for the aws config file.
  */
-export function getAwsConfig(pathToConfig: string = DEFAULT_CONFIG_PATH) {
-  const awsFileName = `${pathToConfig}${pathSeparator}${AWS_FILE_NAME}`;
+export function getAwsConfig(
+  pathToConfig: string = DEFAULT_CONFIG_PATH,
+  fileName: string = AWS_FILE_NAME
+) {
+  const awsFileName = `${pathToConfig}${pathSeparator}${fileName}`;
   return new ConfigFile<AwsConfig>(awsFileName, AwsRequiredProperties);
 }
 
 /**
  * Create a new `ConfigFile` for the given configuration path.
- * @param pathToConfig from which the aws config will be read.
+ * @param pathToConfig from which the project config will be read.
+ * @param fileName of the config file within `pathToConfig`
  * @returns a handler for the project config file.
  */
-export function getProjectConfig(pathToConfig: string = DEFAULT_CONFIG_PATH) {
-  const projectFileName = `${pathToConfig}${pathSeparator}${PROJECT_FILE_NAME}`;
+export function getProjectConfig(
+  pathToConfig: string = DEFAULT_CONFIG_PATH,
+  fileName: string = PROJECT_FILE_NAME
+) {
+  const projectFileName = `${pathToConfig}${pathSeparator}${fileName}`;
   return new ConfigFile<ProjectConfig>(
     projectFileName,
     ProjectRequiredProperties
