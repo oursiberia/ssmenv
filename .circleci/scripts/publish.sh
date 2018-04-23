37ad3a0a63b678edec34111949d77b3dcdbfd40c
Bryan Swift <bryan@siberia.io>
2018-04-23 10:40:52 -0400

    Fix logic for when publish with `--npm-tag` flag happens

--- .circleci/scripts/publish.sh
+++ .circleci/scripts/publish.sh
@@ -10,7 +10,7 @@ VERSION=$(echo $CIRCLE_TAG | cut -c2- | cut -d '-' -f 1)
 # VERSION_SUFFIX extracts by 'splitting' on the hyphen and taking the first
 # part (field in `cut` terms).
 VERSION_SUFFIX=$(echo $CIRCLE_TAG | cut -c2- | cut -d '-' -f 2 -s)
-if [[ ! "__${VERSION_SUFFIX}" == "__" ]]; then
+if [ "__${VERSION_SUFFIX}" == "__" ]; then
   # NPM_TAG defaults to "latest" but uses the value of the take if it is not
   # empty. Emptiness is determined by prefixing and checking if the result
   # matches the prefix exactly. Don't include the --npm-tag for `latest`
